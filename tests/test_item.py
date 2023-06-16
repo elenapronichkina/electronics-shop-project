e1c443ccbb43d7d38cc33097e618111f19eb406f
Elena Pronichkina <prolena39@mail.ru>
2023-06-16 21:49:06 +0300

    добавлены тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+from maim import item1, item2
+#item1 = Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(self):
+    assert item1.calculate_total_price() == 200000.0
+
+
+def test_apply_discount(self):
+    assert item1.apply_discount() == 8000.0
