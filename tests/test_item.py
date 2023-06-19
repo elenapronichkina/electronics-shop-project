98b3d003cde3b4dc82031c83a221f3cc77627e9f
Elena Pronichkina <prolena39@mail.ru>
2023-06-19 22:20:19 +0300

    исправлен test_apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,12 +1,15 @@
 import pytest
 from src.item import Item
-from maim import item1, item2
+#from maim import item1, item2
 #item1 = Item("Смартфон", 10000, 20)
 
 
 def test_calculate_total_price(self):
     assert item1.calculate_total_price() == 200000.0
 
-
 def test_apply_discount(self):
-    assert item1.apply_discount() == 8000.0
+    item3 = Item("Смартфон", 1000, 10)
+    Item.pay_rate = 0.7
+    item3.apply_discount()
+    print(item3.price)
+
