0d66654d0263c003942941c62e214fe5db1fb1e9
Elena Pronichkina <prolena39@mail.ru>
2023-06-25 15:52:02 +0300

    добавлены тесты методов __repr__ и __str__

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,12 +1,13 @@
 import pytest
 from src.item import Item
 #from maim import item1, item2
-item1 = Item("Смартфон", 10000, 20)
+item1 = Item('Смартфон', 10000, 20)
+item2 = Item('Телефон', 5000, 10)
 def test_calculate_total_price(self):
     assert item1.calculate_total_price() == 200000.0
 
 def test_apply_discount():
-    item3 = Item("Смартфон", 1000, 10)
+    item3 = Item('Смартфон', 1000, 10)
     print(item3)
     Item.pay_rate = 0.7
     item3.apply_discount()
@@ -18,3 +19,11 @@ def test_name():
 
 def test_string_to_number():
     assert item1.string_to_number('20') == 20
+
+def test_repr():
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert repr(item2) == "Item('Телефон', 5000, 10)"
+
+def test_str():
+    assert str(item1) == 'Смартфон'
+    assert str(item2) == 'Телефон'
