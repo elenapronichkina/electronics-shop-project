ef374499974d01a524fa55bfab5fcd155a9b63bc
Elena Pronichkina <prolena39@mail.ru>
2023-07-14 18:13:31 +0300

    исправлен метод change_lang(), добавлен  get_language()

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,16 +4,22 @@ class Mixin:
     kb_language = "EN"
     def __init__(self):
         self.language = self.kb_language
-
+    @property
+    def get_language(self):
+        return self.language
     def change_lang(self):
-        self.language = "RU"
+        kb_language = "EN"
+        if self.language == kb_language:
+            self.language = "RU"
+        else:
+            self.language = "EN"
         return self.language
 
 class KeyBoard(Item, Mixin):
     """
     дочерний класс.
     """
-    def __init__(self, name: str, price: float, quantity: int, language: str):
+    def __init__(self, name: str, price: float, quantity: int, language):
         # Вызываем метод базового класса
         super().__init__(name, price, quantity)
         # Дополнительный код
